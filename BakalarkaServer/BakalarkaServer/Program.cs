7268c3d13e43d6ff916f4d24c456d63b1b93fbfa
ondra Pollák <ondra.pollak@gmail.com>
2020-11-21 15:38:08 +0100

    test

---
+++ BakalarkaServer/BakalarkaServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BakalarkaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloo World!");
+        }
+    }
+}
